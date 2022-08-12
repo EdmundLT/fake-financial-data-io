ad3178ca66269349650b44c13297a085052aa538
Edmund Tang <98913678+EdmundLT@users.noreply.github.com>
2022-08-12 17:53:40 -0400

    delete prism

--- client/src/components/Guide/Guide.jsx
+++ client/src/components/Guide/Guide.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import Navbar from "../Navbar";
-import { Prism } from "@mantine/prism";
+// import { Prism } from "@mantine/prism";
 import { demoCode } from "../../data/data";
 const Guide = () => {
   return (
@@ -9,11 +9,11 @@ const Guide = () => {
       <div className="container mx-auto text-center py-4">
         <p className="text-3xl font-bold pb-4">Financial Data</p>
         <p>Get </p>
-        <div className="text-left">
+        {/* <div className="text-left">
           <Prism withLineNumbers language="jsx" colorScheme="dark">
             {demoCode}
           </Prism>
-        </div>
+        </div> */}
       </div>
     </div>
   );
--- client/src/components/Try.jsx
+++ client/src/components/Try.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Prism } from "@mantine/prism";
+// import { Prism } from "@mantine/prism";
 import { demoCode, demoResult } from "../data/data";
 
 const Try = () => {
@@ -11,7 +11,7 @@ const Try = () => {
           Using Javascript <i>fetch API</i> as example:
         </p>
       </div>
-      <div className="text-left px-6">
+      {/* <div className="text-left px-6">
         <Prism withLineNumbers language="jsx" colorScheme="dark">
           {demoCode}
         </Prism>
@@ -20,7 +20,7 @@ const Try = () => {
         <Prism withLineNumbers language="json" colorScheme="dark">
           {demoResult}
         </Prism>
-      </div>
+      </div> */}
       <p className="text-left px-6 text-xl">Congrat! This is your first fetch!</p>
     </div>
   );
